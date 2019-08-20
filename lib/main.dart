a716f8d95d2f402c4ef998231b7a2d8d09173077
Simran Singh <Jyotishka.r7139@gmail.com>
2019-08-21 03:57:42 +0530

    lat, long, accuracy tracking

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,10 @@ import 'package:location/location.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: FireMap(),
       ),
@@ -24,14 +24,29 @@ class FireMap extends StatefulWidget {
 
 class _FireMapState extends State<FireMap> {
   GoogleMapController mapController;
-  Location location = new Location();
+  var currentLocation = LocationData;
+  var location = new Location();
+  var lat, long, accuracy;
+
+  @override
+  void initState() {
+    super.initState();
+    //LocationAccuracy.HIGH;
+    location.onLocationChanged().listen((LocationData currentLocation) {
+      setState(() {
+        lat = currentLocation.latitude;
+        long = currentLocation.longitude;
+        accuracy = currentLocation.accuracy;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         GoogleMap(
-          mapType: MapType.terrain,
+          myLocationButtonEnabled: true,
           initialCameraPosition: CameraPosition(
             target: LatLng(23.554079, 87.278687),
             zoom: 13,
@@ -43,23 +58,21 @@ class _FireMapState extends State<FireMap> {
         ),
         Positioned(
           bottom: 20,
+          left: 20,
           right: 20,
-          child: FloatingActionButton(
-            onPressed: () async {
-              location.onLocationChanged();
-              var pos = await location.getLocation();
-              //location.onLocationChanged();
-              mapController.animateCamera(
-                CameraUpdate.newCameraPosition(
-                  CameraPosition(
-                    target: LatLng(pos.latitude, pos.longitude),
-                    zoom: 15,
-                  ),
-                ),
-              );
-            },
+          top: 480,
+          child: Container(
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(13),
+            ),
+            child: Column(
+              children: <Widget>[Text("LATITUDE: $lat"), Text("LONGITUDE: $long"), Text("ACCURACY: $accuracy")],
+            ),
           ),
-        )
+        ),
       ],
     );
   }
@@ -70,44 +83,3 @@ class _FireMapState extends State<FireMap> {
     });
   }
 }
-
-/*import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
-    );
-  }
-}*/
